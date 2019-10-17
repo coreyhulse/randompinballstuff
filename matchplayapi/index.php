832fa717fe4c5d600ebd69d3e4d88bc2964b3f83
Corey Hulse <corey.hulse@revzilla.com>
2019-10-17 17:24:54 -0400

    MatchPlay Players

---
+++ matchplayapi/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Match Play Participants</title>
+
+
+        <style>
+            .my_text
+            {
+                font-family:    "Courier New", Courier, monospace;
+                font-size:      <?php echo $fontsize;?>px;
+            }
+        </style>
+
+</head>
+
+
+
+<body>
+<div class="my_text">
+<a href=players.php>
+Player List
+</a>
+
+</div>
+</body>
